4bc87d92a66a3cdbae25e8ad5fd5c9478a46b84d
Артур <arturskomarohov@gmail.com>
2021-08-19 18:48:54 +0300

    Добавьте файлы проекта.

---
+++ SampleForReflection/Figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleForReflection
+{
+    class Figure
+    {
+        public string Name { get; set; }
+        public int SideCount { get; set; }
+        public double SideLength { get; set; }
+
+        public void Display(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
