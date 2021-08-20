dcea22dbd85483786e2612ebf286fd1162b623f8
Артур <arturskomarohov@gmail.com>
2021-08-20 20:24:02 +0300

    reflection values

--- SampleForReflection/Program.cs
+++ SampleForReflection/Program.cs
@@ -10,66 +10,7 @@ namespace SampleForReflection
         static void Main(string[] args)
         {
 
-
             //1
-
-            DirectoryInfo projectsDir = new DirectoryInfo($@"G:\C#Projects");
-            var projFolders = projectsDir.EnumerateDirectories();
-            var targetProject = "SampleForFileStream";
-            var targetPrFolder =
-                from proj in projFolders
-                where proj.Name == targetProject
-                select proj;
-
-
-
-            //string path = @"G:\C#Projects\SampleForFileStream\SampleForFileStream\obj\Debug\netcoreapp3.1\SampleForFileStream.dll";
-            //string path = @"G:\C#Projects\SampleForSerialization\SampleForSerialization\bin\Debug\netcoreapp3.1\SampleForSerialization.dll"
-            //string path = Path.Combine("G:", "C#Projects", "SampleForFileStream", "SampleForFileStream", "obj", "Debug", "netcoreapp3.1", "SampleForFileStream.dll");
-            string path = Path.Combine("G:", "C#Projects", "SampleForSerialization", "SampleForSerialization", "obj", "Debug", "netcoreapp3.1", "SampleForSerialization.dll");
-            Console.WriteLine(AssemblyName.GetAssemblyName(path));
-            
-            //Console.WriteLine($"это текущая сборка и ее адрес {AppDomain.CurrentDomain.BaseDirectory}"); 
-
-            Assembly assem = Assembly.LoadFrom(path);
-            var assemTypes = assem.GetTypes();
-            var assemType = assem.GetType("SampleForSerialization.DataSerializer");
-            foreach (var item in assemType.GetMethods())
-            {
-                Console.WriteLine(item.Name);
-            }
-            foreach (var type in assemTypes)
-            {
-                Console.WriteLine(type);
-            }
-            var props = assemType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var item in props)
-            {
-                Console.WriteLine(item.Name);
-            }
-            var methods = assemType.GetMethods(BindingFlags.Public | BindingFlags.Static);
-            foreach (var item in methods)
-            {
-                Console.WriteLine(item.Name);
-            }
-            object obj1 = Activator.CreateInstance(assemType);
-
-            
-
-
-            //ConstructorInfo cons = assemType.GetConstructor(new Type[] { });
-            //object persReflected = cons.Invoke(new object[] {});
-
-
-            //Console.WriteLine($"invoked {persReflected}");
-            //Person obj = (Person)persReflected;
-            var psAge = assemType.GetProperty("Age");
-            
-            //Console.WriteLine(persAge.GetValue(persReflected));
-            //var props = targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-
-
             Type myType = typeof(Figure);
             var myProperties = myType.GetProperties();
             var some = myType.GetMembers();
@@ -85,6 +26,82 @@ namespace SampleForReflection
 
             //4
             //Display(triangle);
+
+            //string path = @"G:\C#Projects\SampleForFileStream\SampleForFileStream\obj\Debug\netcoreapp3.1\SampleForFileStream.dll";
+            //string path = @"G:\C#Projects\SampleForSerialization\SampleForSerialization\bin\Debug\netcoreapp3.1\SampleForSerialization.dll"
+            //string path = Path.Combine("G:", "C#Projects", "SampleForFileStream", "SampleForFileStream", "obj", "Debug", "netcoreapp3.1", "SampleForFileStream.dll");
+
+            // ПУТЬ К ИСКОМОЙ СБОРКЕ
+            string path = Path.Combine("G:", "C#Projects", "SampleForSerialization", "SampleForSerialization", "obj", "Debug", "netcoreapp3.1", "SampleForSerialization.dll");
+            Console.WriteLine(AssemblyName.GetAssemblyName(path));
+            //Console.WriteLine($"это текущая сборка и ее адрес {AppDomain.CurrentDomain.BaseDirectory}"); 
+
+            //ЗАГРУЗКА ИСКОМОЙ СБОРКИ
+            Assembly assem = Assembly.LoadFrom(path);
+
+            
+            var assemTypes = assem.GetTypes();
+            var assemType = assem.GetType("SampleForSerialization.Person");
+
+            
+            //ТИПЫ
+            foreach (var type in assemTypes)
+            {
+                Console.WriteLine(type);
+            }
+
+            //СВОЙСТВА
+            var props = assemType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var prop in props)
+            {
+                Console.WriteLine(prop.Name);
+            }
+            //МЕТОДЫ 
+            var methods = assemType.GetMethods(BindingFlags.Public | BindingFlags.Static);
+            foreach (var item in methods)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            //ПРИВАТНЫЕ МЕТОДЫ 
+            var privMeths = assemType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var prMeth in privMeths)
+            {
+                Console.WriteLine(prMeth.Name);
+            }
+
+            //ИНИЦИАЦИЯ 
+            object obj1 = Activator.CreateInstance(assemType);
+
+            //ПРИСВАИВАНИЕ СВОЙСТВАМ ЗНАЧЕНИЯ
+            PropertyInfo propName = assemType.GetProperty("Name");
+            propName.SetValue(obj1, "Восьмигранник", null);
+            Console.WriteLine($"{propName.Name} : {propName.GetValue(obj1)}");
+
+            PropertyInfo propAge = assemType.GetProperty("SideCount");
+            Console.WriteLine($"{propAge.Name} : {propAge.GetValue(obj1)}") ;
+
+            ConstructorInfo cons = assemType.GetConstructor(new Type[] { });
+            object persReflected = cons.Invoke(new object[] {"dkfj",4 });
+
+
+            // Console.WriteLine($"invoked {persReflected}");
+            var privMeth = assemType.GetMethod("StrangeName", BindingFlags.NonPublic);
+            Console.WriteLine(privMeth.Name);
+
+
+            /*Type type4 = Type.GetType("SampleForSerialization.Figure", false, true);
+            Console.WriteLine(type4.FullName);*/
+            
+           
+
+
+
+
+
+
+
+
         }
         private static void Display<T>(T obj)
         {
