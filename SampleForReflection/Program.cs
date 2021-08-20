83ebd96611f7795353a0256eae6cf80ec23f8d30
Артур <arturskomarohov@gmail.com>
2021-08-20 18:14:21 +0300

    assembly loadfrom

--- SampleForReflection/Program.cs
+++ SampleForReflection/Program.cs
@@ -1,19 +1,81 @@
 ﻿using System;
 using System.Reflection;
+using System.IO;
+using System.Linq;
 
 namespace SampleForReflection
 {
     class Program
     {
         static void Main(string[] args)
-        {//1
+        {
+
+
+            //1
+
+            DirectoryInfo projectsDir = new DirectoryInfo($@"G:\C#Projects");
+            var projFolders = projectsDir.EnumerateDirectories();
+            var targetProject = "SampleForFileStream";
+            var targetPrFolder =
+                from proj in projFolders
+                where proj.Name == targetProject
+                select proj;
+
+
+
+            //string path = @"G:\C#Projects\SampleForFileStream\SampleForFileStream\obj\Debug\netcoreapp3.1\SampleForFileStream.dll";
+
+            string path = Path.Combine("G:", "C#Projects", "SampleForFileStream", "SampleForFileStream", "obj", "Debug", "netcoreapp3.1", "SampleForFileStream.dll");
+            Console.WriteLine(AssemblyName.GetAssemblyName(path));
+
+            //Console.WriteLine($"это текущая сборка и ее адрес {AppDomain.CurrentDomain.BaseDirectory}"); 
+
+            Assembly assem = Assembly.LoadFrom(path);
+            var assemTypes = assem.GetTypes();
+            var assemType = assem.GetType("SampleForFileStream.Person");
+            foreach (var item in assemType.GetMethods())
+            {
+                Console.WriteLine(item.Name);
+            }
+            foreach (var type in assemTypes)
+            {
+                Console.WriteLine(type);
+            }
+            var props = assemType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var item in props)
+            {
+                Console.WriteLine(item.Name);
+            }
+            var methods = assemType.GetMethods(BindingFlags.Public | BindingFlags.Static);
+            foreach (var item in methods)
+            {
+                Console.WriteLine(item.Name);
+            }
+            object obj1 = Activator.CreateInstance(assemType);
+
+            
+
+
+            //ConstructorInfo cons = assemType.GetConstructor(new Type[] { });
+            //object persReflected = cons.Invoke(new object[] {});
+
+
+            //Console.WriteLine($"invoked {persReflected}");
+            //Person obj = (Person)persReflected;
+            var psAge = assemType.GetProperty("Age");
+            
+            //Console.WriteLine(persAge.GetValue(persReflected));
+            //var props = targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+
+
             Type myType = typeof(Figure);
             var myProperties = myType.GetProperties();
-            var myMethods = myType.GetMethods();
+            var some = myType.GetMembers();
 
             //2
-            var triangle = new Figure() { Name = "Triangle", SideCount = 5, SideLength = 5 };
-            var myType2 = triangle.GetType();
+            //var triangle = new Figure() { Name = "Triangle", SideCount = 7, SideLength = 5 };
+            //var myType2 = triangle.GetType();
 
 
             //3
@@ -21,7 +83,7 @@ namespace SampleForReflection
 
 
             //4
-            Display(triangle);
+            //Display(triangle);
         }
         private static void Display<T>(T obj)
         {
@@ -30,6 +92,7 @@ namespace SampleForReflection
             foreach (var property in properties)
             {
                 var s = property.GetValue(obj);
+                Console.WriteLine(s);
             }
         }
     }
